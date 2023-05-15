d768890a3a9e0bd535587e135486c5c4ce606cc6
GabrieleConte <gabryconte90@gmail.com>
2023-05-15 16:33:04 +0200

    Initial Commit

---
+++ nailsCenterAppNative/nailsCenterAppNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nailsCenterAppNativeApp.swift
+//  nailsCenterAppNative
+//
+//  Created by Gabriele Conte on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct nailsCenterAppNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
