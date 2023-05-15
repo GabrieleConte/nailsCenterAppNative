391e6921da1ba8fd1d3365b01152eee2c1fb9a4a
GabrieleConte <gabryconte90@gmail.com>
2023-05-15 17:58:56 +0200

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'nailsCenterAppNative' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for nailsCenterAppNative
+pod 'SCSDKCameraKit', '1.21.0'
+pod 'SCSDKCameraKitReferenceSwiftUI', '1.21.0'
+pod 'SCSDKCameraKitReferenceUI', '1.21.0'
+end
--- nailsCenterAppNative/ContentView.swift
+++ nailsCenterAppNative/ContentView.swift
@@ -6,17 +6,14 @@
 //
 
 import SwiftUI
+import SCSDKCameraKit
+import SCSDKCameraKitReferenceUI
+import SCSDKCameraKitReferenceSwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        CameraView(cameraController: CameraController(sessionConfig: SessionConfig(applicationID: "469231f3-aa68-4dc8-ba68-e989df71b1fe", apiToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjgxNjUzNTY2LCJzdWIiOiI0NjkyMzFmMy1hYTY4LTRkYzgtYmE2OC1lOTg5ZGY3MWIxZmV-U1RBR0lOR35iODk3N2MzMi02NzkwLTRkOWYtOTUyMC1iMGU0Nzc1YmExNmQifQ.CLk2Utb8AtBb3Yuz_mqm_7bxqH4WOfTNR7PdP2PUYxQ")) )
+            }
 }
 
 struct ContentView_Previews: PreviewProvider {
